74d6987da15aef751a5c81d4d5a5621b48954b22
Koushik Krishnan <krishnan.koushik@gmail.com>
2018-01-21 01:02:19 -0500

    added frontend stuff

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, render_template
 import urlparse
 import psycopg2
 from psycopg2.extras import DictCursor
@@ -24,9 +24,56 @@ def test():
 
 @app.route('/ping')
 def ping():
-	return "pong"
+	return "atlanta28ne3"
 
-@app.route('/registration', methods=['POST'])
+@app.route('/', methods=['GET'])
+def index():
+	return app.send_static_file('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def teacher_login():
+	if request.method == 'POST':
+		if validate_creds(request.form['email'], request.form['password']):
+
+			query = 'select * from tutors where email=%s'
+			with db.cursor(cursor_factory = psycopg2.extras.DictCursor) as cur:
+				try:
+					cur.execute(query, (request.form['email'],))
+					row = cur.fetchone()
+					name = row['name']
+					college = row['college']
+					subjects = row['subjects']
+					email = row['email']
+					return render_template('teacher-profile.html', name=name, email=email, college=college, subjects=subjects)
+				except psycopg2.DatabaseError:
+					return app.send_static_file('teacher-login.html')
+		else:
+			return app.send_static_file('teacher-login.html')
+	return app.send_static_file('teacher-login.html')
+
+@app.route('/update', methods=['GET', 'POST'])
+def teacher_update():
+	if request.method == 'POST':
+		if validate_creds(request.form['email'], request.form['password']):
+
+			query = 'select * from tutors where email=%s'
+			with db.cursor(cursor_factory = psycopg2.extras.DictCursor) as cur:
+				try:
+					cur.execute(query, (request.form['email'],))
+					row = cur.fetchone()
+					name = row['name']
+					college = row['college']
+					subjects = row['subjects']
+					email = row['email']
+					return render_template('update-teacher-profile.html', name=name, email=email, college=college, subjects=subjects)
+				except psycopg2.DatabaseError:
+					return app.send_static_file('teacher-login.html')
+		else:
+			return app.send_static_file('teacher-login.html')
+	return app.send_static_file('teacher-login.html')
+
+
+@app.route('/api/registration', methods=['POST'])
 def register():
 	body = request.form
 
@@ -38,7 +85,7 @@ def register():
 
 	return insert_teacher_into_database(name, email, college, subjects, password)
 
-@app.route('/update_teacher_profile', methods=['POST'])
+@app.route('/api/update', methods=['POST'])
 def update():
 	body = request.form
 
@@ -49,7 +96,7 @@ def update():
 
 	return update_teacher(email, college, subjects, password)
 
-@app.route('/search', methods=['GET'])
+@app.route('/api/search', methods=['GET'])
 def search():
 	query = request.args.get('query')
 	query.replace(" ", "")
@@ -95,7 +142,7 @@ def update_teacher(email, college, subjects, password):
 	if subjects != None:
 		if count != 0:
 			query += ","
-		query += " subjects = '{}'".format(subjects)
+		query += " subjects = '{%s}'" % (subjects)
 		count += 1
 	if password != None:
 		if count != 0:
@@ -107,10 +154,23 @@ def update_teacher(email, college, subjects, password):
 			cur.execute(str(query) + ' WHERE email = %s', (email,))
 		except psycopg2.DatabaseError as e:
 			db.rollback()
-			return jsonify({"success": False})
+			return jsonify({"success": False, "message": str(e)})
 		else:
 			db.commit()
 			return jsonify({"success": True})
+
+def validate_creds(email, password):
+	query = 'select count(*) from tutors where email=%s and password=%s'
+	with db.cursor(cursor_factory = psycopg2.extras.DictCursor) as cur:
+		try:
+			cur.execute(query, (email, password))
+			row = cur.fetchone()
+			if row['count'] != 1:
+				return False
+			else:
+				return True
+		except psycopg2.DatabaseError:
+			return False
 
 
 if __name__ == "__main__":
